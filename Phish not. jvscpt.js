38d2f39c267ae24f50494cde9986b7a1050423a8
CarloDallaFonte <carloxd.cc@gmail.com>
2025-07-12 20:36:35 -0600

    Delete Phish not. jvscpt.js

--- Phish not. jvscpt.js
+++
@@ -1,8 +0,0 @@
-// You can add JavaScript functionalities here later if needed.
-// For example, smooth scrolling, form validation, animations, etc.
-
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('PhishNot website loaded successfully!');
-});
-
-
